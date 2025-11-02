903b0ebb0b524722096c897c432ff238344a11a7
JeIIIa <jeiiiaa@mail.ru>
2025-11-02 21:12:21 +0300

    Create mydc.py
    C

---
+++ mydc.py
@@ -0,0 +1,16 @@
+class Timer:
+
+    def __init__(self, name_func):
+        self.func = name_func
+
+    def __call__(self, *args, **kwargs):
+        print("Запущена функция")
+        result = self.func(*args, **kwargs)
+        print("Функция завершена")
+        return result
+    
+
+@Timer
+def my_func(a, b):
+    return a + b
+
